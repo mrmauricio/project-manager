cdaef18518cbcabfb6f88a81082167d5abe88ae9
mrmauricio <mendes-mauricio@outlook.com>
2019-08-14 21:19:06 +0100

    Added User info to Redux

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -4,10 +4,12 @@ import projects from './projects/reducer';
 import users from './users/reducer';
 import technologies from './technologies/reducer';
 import auth from './auth/reducer';
+import user from './user/reducer';
 
 export default combineReducers({
     projects,
     users,
     technologies,
     auth,
+    user,
 });
